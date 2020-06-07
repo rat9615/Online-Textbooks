b55f97d4a89ecd40180a09370e90121a318b16ac
rohanr <rohanrocket4@gmail.com>
2020-06-07 22:14:08 +0530

    Move Filepond and JQuery UI Autocomplete to seperate files

---
+++ public/js/demo/jquery-ui-autocomplete.js
@@ -0,0 +1,48 @@
+// load jQuery UI Autcomplete plugin
+$(function () {
+  // function popover
+  function popoverHtml() {
+    $('[data-toggle="popover"]').popover({
+      animation: true,
+      html: true,
+      trigger: 'hover click',
+    });
+  }
+
+  popoverHtml();
+  $('#autocomplete')
+    .autocomplete({
+      source(req, res) {
+        $.ajax({
+          url: `/search/books/${req.term}`,
+          search: req.term,
+          success(data) {
+            res(data);
+          },
+        });
+      },
+      select(_event, ui) {
+        $.ajax({
+          url: `/books/${ui.item.value}`,
+          success(data) {
+            $('#downloadBooks').html(data);
+            popoverHtml();
+          },
+        });
+      },
+      response(_event, ui) {
+        if (!ui.content.length) {
+          const noResult = { value: '', label: 'No matches found!' };
+          ui.content.push(noResult);
+        }
+      },
+    })
+    .on('keydown', function (event, ui) {
+      if (event.keyCode === 13) {
+        // prevent form submission
+        event.preventDefault();
+        // close the autocomplete
+        $('#autocomplete').autocomplete('close');
+      }
+    });
+});
